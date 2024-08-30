b9ace19b5eb361a52ffa4db34354e1e2b19f67cb
Haekz <b.nicovani@duocuc.cl>
2024-08-29 21:17:43 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyApps',
+  webDir: 'www'
+};
+
+export default config;
