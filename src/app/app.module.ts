a2b7b48674413d958678d7dce9c663ebf65ca3a1
Haekz <b.nicovani@duocuc.cl>
2024-08-29 22:16:36 -0400

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';  // Asegúrate de importar FormsModule
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { LoginComponent } from './pages/login/login.component';
+import { SearchResultsComponent } from './pages/search-results/search-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    // Asegúrate de que todos los componentes necesarios estén aquí
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,  // Asegúrate de que esto esté incluido
+    IonicModule.forRoot(),  // Asegúrate de que IonicModule esté aquí
+    AppRoutingModule,
+    FormsModule  // Asegúrate de incluir FormsModule aquí
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
