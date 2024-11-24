1d4fbb620549353c7add6831f8c4c0bb4a6116fc
Rafu <ra.quichiz@duocuc.cl>
2024-11-24 15:54:58 -0300

    assa

--- src/app/public/publicacion.service.ts
+++ src/app/public/publicacion.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PublicacionService {
-  private apiUrl = 'http://localhost:3000/publicacion';
+  private apiUrl = 'http://192.168.1.119:3000/publicacion';
 
   constructor(private http: HttpClient) {}
 
